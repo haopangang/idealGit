120c95643a056983e7e8a2709d925df164f19f90
haopangang <791845648@qq.com>
2017-08-01 16:41:02 +0800

    添加employee类

---
+++ src/com/haopangang/beans/Employee.java
@@ -0,0 +1,25 @@
+package com.haopangang.beans;
+
+/**
+ * Created by haopangang on 2017/8/1.
+ */
+public class Employee {
+    private int id;
+    private String ename;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+}
