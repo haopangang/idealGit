232fca5334d5ce158ee68e09ba78347d58467371
haopangang <791845648@qq.com>
2017-08-01 16:34:41 +0800

    添加User类

--- src/com/haopangang/SSM/HelloGit.java
+++ src/com/haopangang/SSM/HelloGit.java
@@ -1,7 +1,19 @@
 package com.haopangang.SSM;
 
+import com.haopangang.beans.User;
+
 /**
  * Created by haopangang on 2017/8/1.
  */
 public class HelloGit {
+    public static void main(String args[]) {
+
+        System.out.println("Hello git");
+        User user = new User();
+        user.setName("郝攀刚");
+        user.setId(1);
+        user.setSex("男");
+
+        System.out.println(user.toString());
+    }
 }
